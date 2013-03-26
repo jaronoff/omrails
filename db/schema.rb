8438ef8d0f0ee1d683d83d16cbd22981c91b41df
Josh Aronoff <aronoff@gmail.com>
2013-03-25 21:29:33 -0400

    Add Pins Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130324182307) do
+ActiveRecord::Schema.define(:version => 20130326003728) do
 
   create_table "pins", :force => true do |t|
     t.string   "description"
