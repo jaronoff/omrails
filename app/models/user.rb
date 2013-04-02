b0b69b2e0887508ca349c4190097beb5ed2b89c8
Josh Aronoff <aronoff@gmail.com>
2013-04-01 22:01:24 -0400

    Add user model with Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 devise devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable #, :recoverable
 
   # Setup accessible (or protected) attributes for your model
-attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
 
   has_many :pins
